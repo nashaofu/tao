c701110a9e0067b81a6769a9d6c1eb2eeed2278a
nashaofu <diaocheng@outlook.com>
2020-12-01 22:04:56 +0800

    init 初始化项目配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Tao',
+  description: '道可道，非常道。名可名，非常名。',
+  base: '/tao/',
+  themeConfig: {
+    sidebar: 'auto',
+    smoothScroll: true,
+    repo: 'nashaofu/tao',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    nav: [
+      {
+        text: '道德经',
+        link: '/道德经'
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroImage: /hero.png
+actionText: 道德经 →
+actionLink: /道德经
+footer: MIT Licensed | Copyright © 2020 nashaofu
+---
+
+有道无术，术尚可求也。有术无道，止于术。
+
+以道驭术，术必成。离道之术，术必衰。
+
+道为术之灵，术为道之体；以道统术，以术得道。
